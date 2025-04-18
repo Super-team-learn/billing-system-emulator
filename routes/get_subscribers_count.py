425e5c3a779e659e7a1ed9c63b334c7daaa3f8c9
Khokhlov Kirill <hkp69@yandex.ru>
2025-04-18 18:11:02 +0300

    feat: change reponse type

--- routes/get_subscribers_count.py
+++ routes/get_subscribers_count.py
@@ -3,14 +3,16 @@ from random import randint
 
 def get_subscribers_count(lat, lon, radius):
     '''
-
-    :param lat:
-    :param lon:
-    :param radius:
+    Эмлулирует работу биллинговой системы. Выдает количество абонентов, подключенных к сети и находящихся в пределах радиуса точки
+    :param lat: Широта точки
+    :param lon: Долгота точки
+    :param radius: Радиус
     :return:
     '''
     lat = float(lat)
     lon = float(lon)
     radius = float(radius)
 
-    return randint(10*int(lat)*int(radius), 1000*int(lon)*int(radius))
+    return {
+        'subscribers_count': randint(10*int(lat)*int(radius), 1000*int(lon)*int(radius))
+    }
